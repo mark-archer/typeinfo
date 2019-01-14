374206b1463d1dac90da94bc8e8be1bc559da227
Mark Archer <mark@ubo.us>
2019-01-13 19:08:10 -0800

    add simple type for number

--- src/typeinfo.ts
+++ src/typeinfo.ts
@@ -1,9 +1,34 @@
+import { isNumber } from "util";
+
 //import { stringify, copy } from "./utils";
 
+// export type ValidateFnType = (x:any) => any; // should throw an error if invalid
+// export type MatchFnType = (x:any) => boolean;
+// export type StringifyFnType = (x:any) => string;
+// export type ParseFnType = (x:string) => any;
+// export type CastFnType = (x:any) => any;
 
+// export type TypeInfo = {
+//   id?:string
+//   name?:string
+//   validate?:ValidateFnType
+//   match?:MatchFnType
+//   stringify?:StringifyFnType
+// }
 
-export function valueType() {
-  return {
-    
-  }
+// export function valueType() {
+//   return {
+
+//   }
+// }
+
+export const Num = {
+  id: 'Num',
+  name: 'Num',
+  validate: (x:any) => {
+    if(!isNumber(x)) throw 'Invalid Num';
+    return x;
+  },
+  cast: (x:any) => Number(x),
+  match: isNumber
 }
--- tests/typeinfo_spec.ts
+++ tests/typeinfo_spec.ts
@@ -1,9 +1,48 @@
-import { valueType } from '../src/typeinfo'
+import { Num } from '../src/typeinfo'
 
 var should = require('should');
 
 describe('typeinfo', () => {
-  it('should allow creating value types', () => {
-    valueType()
-  })  
+
+  describe('Num', () => {
+
+    describe('validate', () => {
+      it('should return values that successfully validate', () => {
+        Num.validate(0).should.equal(0);
+        Num.validate(1).should.equal(1);      
+      })
+  
+      it('should throw an error for values that do not validate', () => {
+        should(() => Num.validate('1')).throw();
+        should(() => Num.validate(true)).throw();
+        should(() => Num.validate([1])).throw();
+        should(() => Num.validate({})).throw();
+        should(() => Num.validate(null)).throw();
+        should(() => Num.validate(undefined)).throw();
+      })  
+    })
+
+    describe('cast', () => {
+      it('should allow casting values to numbers', () => {
+        Num.cast('1').should.equal(1);
+        Num.cast('a').should.eql(NaN);
+      })
+    })
+
+    describe('match', () => {
+      it('should return true for values that match', () => {
+        Num.validate(0).should.equal(0);
+        Num.validate(1).should.equal(1);
+      })
+
+      it('should return false for values that do not', () => {
+        Num.match('1').should.equal(false);
+        Num.match(true).should.equal(false);
+        Num.match([1]).should.equal(false);
+        Num.match({}).should.equal(false);
+        Num.match(null).should.equal(false);
+        Num.match(undefined).should.equal(false);
+      })
+    })
+  })
 })
